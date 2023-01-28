8e459fa6ea0e184716146d1ad957ba8c56b5bc7a
ralismark <13449732+ralismark@users.noreply.github.com>
2023-01-28 14:29:44 +1100

    Mute

--- hm/desktop-environment/sway.nix
+++ hm/desktop-environment/sway.nix
@@ -94,6 +94,7 @@ in
 
           # media buttons
           # TODO make these work
+          XF86AudioMute = "exec ${pkgs.pulseaudio}/bin/pactl set-sink-mute @DEFAULT_SINK@ toggle";
           XF86AudioRaiseVolume = "exec ${pkgs.pulseaudio}/bin/pactl set-sink-volume @DEFAULT_SINK@ +10%";
           XF86AudioLowerVolume = "exec ${pkgs.pulseaudio}/bin/pactl set-sink-volume @DEFAULT_SINK@ -10%";
 
